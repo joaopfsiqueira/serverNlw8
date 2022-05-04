2fb0a5e630a7100919ce4d17f56b378db707a8b3
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-04 15:22:08 -0300

    feat: realocando rotas

---
+++ src/routes.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
+
+export const routes = express.Router()
+
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "58df4c74eb5822",
+      pass: "ecf382181a082b"
+    }
+  });
+
+
+routes.post('/feedbacks', async (req, res) => {
+    //tirando os valores do body
+    const { type, comment, screenshot } = req.body;
+
+    //comando para fazer um insert dentro do bd: prism.nomedoDb.create({}) dentro do create, existe 2 tipos de opção, data ou select.
+    //data passa os dados que serão criados e o select retorna os dados que serão criados.
+        const feedback = await prisma.feedBack.create({
+            data : {
+                type,
+                comment,
+                screenshot,
+            }     
+        })
+
+       await transport.sendMail({
+            from: "Equipe Feedget <oi@feedget.com>",
+            to: "João Siqueira <joaopfsiqueiraa@gmail.com>",
+            subject: "Novo feedback",
+            html: [
+                `<div style="font-family: sans-serif; font-size: 16px color: #111";> `,
+                `<p>Tipo do feedback: ${type}</p>`,
+                `<p>Comentario: ${comment}</p>`,
+                `</div>`
+            ].join('\n')
+        })
+        
+        return res.status(201).json({data : feedback});
+    }) 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import { prisma } from './prisma';
-import nodemailer from 'nodemailer'
+import { routes } from './routes';
 
 
 const app = express();
@@ -9,44 +8,8 @@ const app = express();
 app.use(express.json())
 
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "58df4c74eb5822",
-      pass: "ecf382181a082b"
-    }
-  });
+app.use(routes)
 
-
-app.post('/feedbacks', async (req, res) => {
-    //tirando os valores do body
-    const { type, comment, screenshot } = req.body;
-
-    //comando para fazer um insert dentro do bd: prism.nomedoDb.create({}) dentro do create, existe 2 tipos de opção, data ou select.
-    //data passa os dados que serão criados e o select retorna os dados que serão criados.
-        const feedback = await prisma.feedBack.create({
-            data : {
-                type,
-                comment,
-                screenshot,
-            }     
-        })
-
-       await transport.sendMail({
-            from: "Equipe Feedget <oi@feedget.com>",
-            to: "João Siqueira <joaopfsiqueiraa@gmail.com>",
-            subject: "Novo feedback",
-            html: [
-                `<div style="font-family: sans-serif; font-size: 16px color: #111";> `,
-                `<p>Tipo do feedback: ${type}</p>`,
-                `<p>Comentario: ${comment}</p>`,
-                `</div>`
-            ].join('\n')
-        })
-        
-        return res.status(201).json({data : feedback});
-    }) 
 
 app.listen(3333, () => {
         console.log('http://localhost:3333')
