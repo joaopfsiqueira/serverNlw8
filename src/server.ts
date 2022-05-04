bc56433f9a6fc3f623891b0875a791768a780348
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-04 15:19:26 -0300

    feat: criando função de enviar e-mail

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer'
 
 
 const app = express();
 
 //avisando o express que vamos receber um json
 app.use(express.json())
+
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "58df4c74eb5822",
+      pass: "ecf382181a082b"
+    }
+  });
 
 
 app.post('/feedbacks', async (req, res) => {
@@ -19,9 +30,21 @@ app.post('/feedbacks', async (req, res) => {
                 type,
                 comment,
                 screenshot,
-            }
+            }     
         })
 
+       await transport.sendMail({
+            from: "Equipe Feedget <oi@feedget.com>",
+            to: "João Siqueira <joaopfsiqueiraa@gmail.com>",
+            subject: "Novo feedback",
+            html: [
+                `<div style="font-family: sans-serif; font-size: 16px color: #111";> `,
+                `<p>Tipo do feedback: ${type}</p>`,
+                `<p>Comentario: ${comment}</p>`,
+                `</div>`
+            ].join('\n')
+        })
+        
         return res.status(201).json({data : feedback});
     }) 
 
