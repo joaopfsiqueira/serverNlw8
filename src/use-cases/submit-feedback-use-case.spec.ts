6fc8cbfe654a22e96ed3c2e0743b6679781ddf02
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-04 22:22:58 -0300

    feat: melhorando jest

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -4,10 +4,16 @@ import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
 //it significa "deveria", ai colocamos um nome ao it, retornamos uma function callback e dentro dele o test.
 describe('SubmitFeedback', () => {
-    it('Shoud be able to submit a feedback', () => {
+    it('Shoud be able to submit a feedback', async () => {
         const submitFeedback = new SubmitFeedbackUseCase(
             { create: async() => {}},
             { sendMail: async () => {}}
         )
+
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'Example comment',
+            screenshot: 'data:image/png;base64, 323276832gdsf67sd78fds687f67s8d',
+        })).resolves.not.toThrow();
     })
 });
