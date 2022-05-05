0c3e022d477696c53591808de3e4189befd7d819
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-04 22:32:25 -0300

    feat: usando cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { routes } from './routes';
+import cors from 'cors'
 
 
 const app = express();
+
+//cors é basicamente uma forma de controle de segurança.
+//Limitando quais endereços podem usar o nosso produto.
+//nesse caso, todos os acessos estão abertos, caso contrário ninguém acessaria.
+app.use(cors());
 
 //avisando o express que vamos receber um json
 app.use(express.json())
