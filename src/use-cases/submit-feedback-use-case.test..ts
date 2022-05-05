41a16c9a5f6c02c8d44ccd41034b547a4bf96848
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-04 22:13:45 -0300

    feat: configurando jest

--- src/use-cases/submit-feedback-use-case.test..ts
+++ src/use-cases/submit-feedback-use-case.test..ts
@@ -1,1 +1,13 @@
-test('')
+//describe coloca um nome para o feedback, que sempre recebe uma function callback
+
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+//it significa "deveria", ai colocamos um nome ao it, retornamos uma function callback e dentro dele o test.
+describe('SubmitFeedback', () => {
+    it('Shoud be able to submit a feedback', () => {
+        const submitFeedback = new SubmitFeedbackUseCase(
+            { create: async() => {}},
+            { sendMail: async () => {}}
+        )
+    })
+});
