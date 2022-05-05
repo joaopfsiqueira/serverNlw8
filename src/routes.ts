5ab5930a2edfe91479b2b666527e632237d3358b
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-04 21:44:58 -0300

    feat: botão de submit

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router()
 
@@ -21,19 +23,30 @@ routes.post('/feedbacks', async (req, res) => {
 
     //comando para fazer um insert dentro do bd: prism.nomedoDb.create({}) dentro do create, existe 2 tipos de opção, data ou select.
     //data passa os dados que serão criados e o select retorna os dados que serão criados.
-        const feedback = 
 
-       await transport.sendMail({
-            from: "Equipe Feedget <oi@feedget.com>",
-            to: "João Siqueira <joaopfsiqueiraa@gmail.com>",
-            subject: "Novo feedback",
-            html: [
-                `<div style="font-family: sans-serif; font-size: 16px color: #111";> `,
-                `<p>Tipo do feedback: ${type}</p>`,
-                `<p>Comentario: ${comment}</p>`,
-                `</div>`
-            ].join('\n')
-        })
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository
+        )
+~
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot
+        });
+
+
+    //    await transport.sendMail({
+    //         from: "Equipe Feedget <oi@feedget.com>",
+    //         to: "João Siqueira <joaopfsiqueiraa@gmail.com>",
+    //         subject: "Novo feedback",
+    //         html: [
+    //             `<div style="font-family: sans-serif; font-size: 16px color: #111";> `,
+    //             `<p>Tipo do feedback: ${type}</p>`,
+    //             `<p>Comentario: ${comment}</p>`,
+    //             `</div>`
+    //         ].join('\n')
+    //     })
         
-        return res.status(201).json({data : feedback});
+        return res.status(201).send()
     }) 
