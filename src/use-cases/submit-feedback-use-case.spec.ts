b7613f92062183c45c143e4a1cef76997ca9ec0b
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-05-04 22:30:11 -0300

    feat: usando spy no jest

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -2,12 +2,17 @@
 
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
+
+//criando uma função espiã, que não tem funcionalidade nenhuma, mas nos permite saber quando essa função foi chamada ou não.
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
 //it significa "deveria", ai colocamos um nome ao it, retornamos uma function callback e dentro dele o test.
 describe('SubmitFeedback', () => {
     it('Shoud be able to submit a feedback', async () => {
         const submitFeedback = new SubmitFeedbackUseCase(
-            { create: async() => {}},
-            { sendMail: async () => {}}
+            { create: createFeedbackSpy},
+            { sendMail: sendMailSpy}
         )
 
         await expect(submitFeedback.execute({
@@ -15,6 +20,9 @@ describe('SubmitFeedback', () => {
             comment: 'Example comment',
             screenshot: 'data:image/png;base64, 323276832gdsf67sd78fds687f67s8d',
         })).resolves.not.toThrow();
+
+        expect(createFeedbackSpy).toHaveBeenCalledWith();
+        expect(sendMailSpy).toHaveBeenCalledWith();
     });
 
     it('Shoud not be able to submit a feedback without type', async () => {
